6d3480588b00df6f2ae0f81506892112388214aa
liuyang.chen <liuyang.chen@aqara.com>
2024-12-04 16:37:13 +0800

    for bug： https://youtrack.jetbrains.com/issue/KT-73408/IllegalStateException-Argument-CLASS-CLASS-nameAny-modalityOPEN-visibilitypublic-superTypes-must-be-an-array

---
+++ src/wasmWasiMain/kotlin/Person.kt
@@ -0,0 +1,11 @@
+/*
+ * Author: liuyang.chen$
+ * Created: 19/11/24$
+ * Last modified: 19/11/24$
+ * Description: 
+ */
+
+class Person(s: String, i: Int) {
+    var name: String = ""
+    var age: Int = 0
+}
--- src/wasmWasiTest/kotlin/Test.kt
+++ src/wasmWasiTest/kotlin/Test.kt
@@ -2,10 +2,10 @@ import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class WasiTest {
-    @Test
-    fun mainTest() {
-        val monotonicTime1 = wasiMonotonicTime()
-        val monotinicTime2 = wasiMonotonicTime()
-        assertTrue(monotonicTime1 <= monotinicTime2, "Wasi monotonic clock is not monotonic :(")
-    }
+//    @Test
+//    fun mainTest() {
+//        val monotonicTime1 = wasiMonotonicTime()
+//        val monotinicTime2 = wasiMonotonicTime()
+//        assertTrue(monotonicTime1 <= monotinicTime2, "Wasi monotonic clock is not monotonic :(")
+//    }
 }
